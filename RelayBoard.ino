e127a2ec822e2a57c9764ca51b423c80424b0e1d
Stuart <stuartdehaas@gmail.com>
2020-07-18 14:47:12 -0700

    All features implemented. Rev1 complete!

--- RelayBoard.ino
+++ RelayBoard.ino
@@ -17,18 +17,22 @@ const int OUTPUT_PINS[] = {9,8,7,6,5,4,3,A5}; // Maps physical pins to the STATE
 const int BUTTON_PIN = 2; // Pin used for the button (PIN 2 has an interupt thats why I'm using it)
 const int POT_PIN = A4;
 const int SWITCH_PIN = A3;
-const int JOYSTICK_HORZ_PIN = A1;
+const int JOYSTICK_HORZ_PIN = A0;
 const int JOYSTICK_VERT_PIN = A2;
+const int JOYSTICK_BUTT_PIN = A1;
 
 
 // Global info for sequences/patterns
+bool MODE = 0; // 0 -> manual or 1 -> auto
 int SEQ = 0; // The current sequence
-//enum SEQ_NAME{alternate_seq, chase_seq}SEQ;
+bool PRIME_MODE = 0; // Sub mode
+int SEQ_MAP[] = {7,1,2,3,4,5,6,0}; // Map SEQ to joystick positions
 unsigned int BEATS[5] = {500,500,500,500,500}; // holds temporary values from button to calculate period
 int INDEX = 0; // Used to index the BEATS array
 int PERIOD = 500; // Sequence period. The speed we move through the pattern.
 unsigned long BUTT_NEXT_STEP_TIME = 500; // When to make the next change. Compared to the system clock.
 int PRESCALER = 1; // Can be used to slow the period
+bool AUTO_LIGHTS_FLAG = false;
 
 // Global variables related to the button
 unsigned long NEXT_CHECK_TIME = 0; // Next time to check the button state
@@ -38,7 +42,7 @@ int DEBOUNCE_TIME = 10; // Button software debounce time
 unsigned long LAST_PRESS_TIME = 0; // Last time the button was pressed. Based on system clock
 
 void setup() {
-  Serial.begin(115200); // Serial is just used for debugging
+  //Serial.begin(115200); // Serial is just used for debugging
   int i;
   for(i=0; i<NUM_CH; i++){
     pinMode(OUTPUT_PINS[i], OUTPUT);
@@ -48,6 +52,7 @@ void setup() {
   pinMode(SWITCH_PIN, INPUT);
   pinMode(JOYSTICK_HORZ_PIN, INPUT);
   pinMode(JOYSTICK_VERT_PIN, INPUT);
+  pinMode(JOYSTICK_BUTT_PIN, INPUT_PULLUP);
   // Attach an interrupt to the button pin. When a rising edge is detected, sends the program to buttonCatch_ISR
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonCatch_ISR, RISING);
   //fill(1);
@@ -142,22 +147,14 @@ void buttonLogic(){
         PERIOD = PERIOD/5; // Set the system period to the average of the 5 readings
         INDEX = 0;
         BUTT_NEXT_STEP_TIME = millis() + PERIOD*PRESCALER - DEBOUNCE_TIME;
-        Serial.print("Tempo: ");
-        Serial.println(60.0/(float(PERIOD)/1000.0));
+        //Serial.print("Tempo: ");
+        //Serial.println(60.0/(float(PERIOD)/1000.0));
       }
     }
-//    Serial.println(duration);
-//    Serial.print("Period: ");
-//    Serial.println(PERIOD);
-
-//    Serial.print("Next Check Time: ");
-//    Serial.println(BUTT_NEXT_STEP_TIME);
-//    Serial.print("Millis: ");
-//    Serial.println(millis());
   }else if(digitalRead(BUTTON_PIN) == HIGH){
     // Button held high
-    changeSEQ();
-    //Serial.println("Button held high!");
+    MODE = MODE ? 0: 1;
+    //changeSEQ();
   }
   BUTTON_INTERUPT_FLAG = false;
   NEXT_CHECK_TIME = millis() + BUTTON_HOLD_TIME;
@@ -183,7 +180,7 @@ int readJoystick(){
 
   switch(joyState){
     case 0:
-      joyState = 13; // discard value
+      joyState = 13; // joystick in center position
       break;
     case 4:
       joyState = 0;
@@ -214,33 +211,76 @@ int readJoystick(){
       break;
   }
   
-  Serial.println(joyState);
   return joyState;
+}
+
+int joystickLogic(){
+  if( digitalRead(JOYSTICK_BUTT_PIN) == LOW){
+      PRIME_MODE = PRIME_MODE ? 0: 1;
+      return 500;
+  }
+
+  int joyState = readJoystick();
+  if(joyState == 13) return 50; // Joystick in center position
+  static int prevJoyState = 0; 
+
+  if(MODE == 0){ // in Auto Mode
+      if(joyState != SEQ){
+          AUTO_LIGHTS_FLAG = false;
+          SEQ = joyState;
+          changeSEQ();
+      }
+  }else{ // in Manual Mode
+    if(PRIME_MODE){
+        // toggle
+        if(joyState != prevJoyState){
+            STATES[joyState] = STATES[joyState] ? 0: 1;
+            printStates();
+            prevJoyState = joyState;
+        }
+
+    }else{
+        // momentary
+        if(digitalRead(SWITCH_PIN) == HIGH){
+            fill(1);
+            STATES[joyState] = 0;
+        }else{
+            fill(0);
+            STATES[joyState] = 1;
+        }
+        printStates();
+    }
+    
+  }
+  return 50;
 }
 
 void loop() {
   static unsigned long POT_NEXT_STEP_TIME = 0;
+  static unsigned long NEXT_JOY_READ = 0;
+  static unsigned long NEXT_SEQ_TIME = 0;
 
   if(millis() > NEXT_CHECK_TIME) buttonLogic();
+  if(millis() > NEXT_JOY_READ) NEXT_JOY_READ = joystickLogic() + millis();
 
   if(millis() > BUTT_NEXT_STEP_TIME){
     BUTT_NEXT_STEP_TIME = millis() + PERIOD*PRESCALER;
-    if(digitalRead(SWITCH_PIN) == LOW) nextState();
+    if(MODE == 0 && digitalRead(SWITCH_PIN) == LOW) nextState();
   }
 
   if(millis() > POT_NEXT_STEP_TIME){
     int potVal = analogRead(A4);
-    POT_NEXT_STEP_TIME = millis() + (potVal * (1900.0 / 1023.0)) + 50;
-    if(digitalRead(SWITCH_PIN) == HIGH) nextState();
+    POT_NEXT_STEP_TIME = millis() + (potVal * (1950.0 / 1023.0)) + 50;
+    if(MODE == 0 && digitalRead(SWITCH_PIN) == HIGH) nextState();
   }
 
-  if(SEQ == 3){
-    int joyState = readJoystick();
-    if(joyState != 13){
-      fill(0);
-      STATES[joyState] = 1;
-      printStates();
-      delay(50);      
+  if(millis() > NEXT_SEQ_TIME){
+    int potVal = analogRead(A4);
+    NEXT_SEQ_TIME = millis() + (potVal * ((60.0*1000.0) / 1023.0)) + 1000;
+    if(AUTO_LIGHTS_FLAG == true && MODE == 0){
+        SEQ = random(0,6);
+        PRIME_MODE = random(0,2);
+        changeSEQ();
     }
   }
 
--- sequences.ino
+++ sequences.ino
@@ -1,13 +1,15 @@
 void nextState(){
+    //static NEXT_SEQ_TIME = 0;
+    int seq = SEQ_MAP[SEQ];
 
-  switch(SEQ){
+  switch(seq){
     case 0:
       invertStates();
       printStates();
       break;
       
     case 1:
-      chase(0);
+      chase(PRIME_MODE);
       printStates();
       break;
       
@@ -17,15 +19,43 @@ void nextState(){
       break;
 
     case 3:
+      chase(PRIME_MODE);
+      printStates();
       break;
+
+    case 4:
+      chase(PRIME_MODE);
+      printStates();
+        break;
+      
+    case 5:
+      chase(PRIME_MODE);
+      printStates();
+        break;
+
+    case 6:
+        fill(PRIME_MODE);
+        printStates();
+        break;
+
+    case 7:
+        AUTO_LIGHTS_FLAG = true;
+        SEQ = random(0,6);
+        nextState();
+        break;
+
+    default:
+        break;
+
   }
 }
 
 void changeSEQ(){
-  SEQ++;
-  if(SEQ >= 4) SEQ = 0;
+//  SEQ++;
+//  if(SEQ >= 4) SEQ = 0;
+    int seq = SEQ_MAP[SEQ];
 
-  switch(SEQ){
+  switch(seq){
     case 0:
       alternate(0);
       break;
@@ -40,9 +70,33 @@ void changeSEQ(){
       break;
 
     case 3:
-      fill(0);
+      fill(1);
+      STATES[0] = 0;
       break;
+
+    case 4:
+        fill(0);
+        STATES[0] = 1;
+        STATES[1] = 1;
+        break;
       
+    case 5:
+        fill(1);
+        STATES[0] = 0;
+        STATES[1] = 0;
+        break;
+
+    case 6:
+        fill(PRIME_MODE);
+        break;
+
+    case 7:
+        fill(0);
+        break;
+
+    case 8:
+        break;
+
     default:
       break;
   }
